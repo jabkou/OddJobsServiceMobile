2e46c565203a057febcbe29fee80faa8d779eb7d
90kid <dawidkoziej5@wp.pl>
2020-05-08 22:05:51 +0200

    ref code

--- lib/screens/register.dart
+++ lib/screens/register.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:flutterappservice/common/constants.dart';
 import 'package:flutterappservice/screens/login.dart';
@@ -14,6 +16,7 @@ class Register extends StatefulWidget {
 class _RegisterState extends State<Register> {
   final TextEditingController _username = new TextEditingController();
   final TextEditingController _password = new TextEditingController();
+  final TextEditingController _confirmPassword = new TextEditingController();
   final TextEditingController _email = new TextEditingController();
   final TextEditingController _firstName = new TextEditingController();
   final TextEditingController _lastName = new TextEditingController();
@@ -50,50 +53,14 @@ class _RegisterState extends State<Register> {
                     'Register:',
                     style: Theme.of(context).textTheme.display4,
                   ),
-                  TextFormField(
-                    controller: _username,
-                    decoration: InputDecoration(
-                      hintText: 'Login',
-                    ),
-                  ),
-                  TextFormField(
-                    controller: _password,
-                    decoration: InputDecoration(
-                      hintText: 'Password',
-                    ),
-                    obscureText: true,
-                  ),
-                  TextFormField(
-                    controller: _password,
-                    decoration: InputDecoration(
-                      hintText: 'Confirm Password',
-                    ),
-                    obscureText: true,
-                  ),
-                  TextFormField(
-                    controller: _firstName,
-                    decoration: InputDecoration(
-                      hintText: 'Name',
-                    ),
-                  ),
-                  TextFormField(
-                    controller: _lastName,
-                    decoration: InputDecoration(
-                      hintText: 'Surname',
-                    ),
-                  ),
-                  TextFormField(
-                    controller: _email,
-                    decoration: InputDecoration(
-                      hintText: 'e-mail',
-                    ),
-                  ),
-                  TextFormField(
-                    controller: _phoneNumber,
-                    decoration: InputDecoration(
-                      hintText: 'phone number',
-                    ),
-                  ),
+                  _registerFormField(_username, 'Login'),
+                  _registerFormField(_password, 'Password', obscureText: true),
+                  _registerFormField(_confirmPassword, 'Confirm Password',
+                      obscureText: true),
+                  _registerFormField(_firstName, 'Name'),
+                  _registerFormField(_lastName, 'Surname'),
+                  _registerFormField(_email, 'e-mail'),
+                  _registerFormField(_phoneNumber, 'phone number'),
                   SizedBox(
                     height: 24,
                   ),
@@ -101,11 +68,15 @@ class _RegisterState extends State<Register> {
                     color: Colors.cyan,
                     child: Text('REGISTER'),
                     onPressed: () {
-                      //'Future<dynamic>' is not a subtype of type 'bool' ogolnie funkcja zwraca bool ale powinna zwracac future
-                      //dynamic bo jest async ale dziala trzeba bezdie rozwiazac ten problem inaczej
-                      _register();
-                      if (this.succRegister) {
-                        Navigator.pushReplacementNamed(context, '/login');
+                      try {
+                        _checkData();
+                        _register();
+                        if (this.succRegister) {
+                          Navigator.pushReplacementNamed(context, '/login');
+                        }
+                      } on Exception catch (e) {
+                        AlertBox.showAlertDialog(
+                            context, "Różne hasła", e.toString(), "OK");
                       }
                     },
                   ),
@@ -115,6 +86,44 @@ class _RegisterState extends State<Register> {
           ),
         ),
       ),
+    );
+  }
+
+  _checkData() {
+    _confPasswords();
+    final alphanumeric = RegExp(r'^[a-zA-Z0-9]+$');
+    //final passwordRegex = RegExp(r'[^\s]+');
+    final emailRegex =
+        RegExp(r"^[a-zA-Z0-9_!#$%&'*+/=?`{|}~^.-]+@[a-zA-Z0-9.-]+$");
+    final phonRegex = RegExp(r"^[0-9]+$");
+    if (!alphanumeric.hasMatch(_username.text) ||
+        !alphanumeric.hasMatch(_password.text))
+      throw Exception("Nieprawidłowy znak");
+    if (_password.text.length < 3 || _password.text.length > 30)
+      throw Exception("Nieprawidłowa długość hasła");
+    if (_username.text.length < 3 || _username.text.length > 30)
+      throw Exception("Nieprawidłowa długość loginu");
+    if (!emailRegex.hasMatch(_email.text)) throw Exception("Błędny email");
+    if (!phonRegex.hasMatch(_phoneNumber.text) || _phoneNumber.text.length != 9)
+      throw Exception("Błędny numer");
+  }
+
+  _confPasswords() {
+    if (!(_password.text == _confirmPassword.text)) {
+      _password.clear();
+      _confirmPassword.clear();
+      throw Exception("Podane hasła nie są takie same");
+    }
+  }
+
+  _registerFormField(TextEditingController controller, String text,
+      {obscureText = false}) {
+    return new TextFormField(
+      controller: controller,
+      decoration: InputDecoration(
+        hintText: text,
+      ),
+      obscureText: obscureText,
     );
   }
 
@@ -135,22 +144,19 @@ class _RegisterState extends State<Register> {
       },
       body: json.encode(data),
     );
-    if (response.statusCode == 200) {
-      jsonData = json.decode(response.body);
-      if (jsonData.length == 0) {
-        Navigator.of(context).pushAndRemoveUntil(
-            MaterialPageRoute(builder: (BuildContext context) => MyLogin()),
-            (Route<dynamic> route) => false);
-        this.succRegister = true;
-      } else {
-        AlertBox.showAlertDialog(context, "Problemy", response.body, "OK");
-      }
+    if (response.statusCode != 200)
+      throw Exception(
+          "Upss... wystapil problem z nawiazanie polaczenia\n Kod błędu: " +
+              response.statusCode.toString());
+
+    jsonData = json.decode(response.body);
+    if (jsonData.length == 0) {
+      Navigator.of(context).pushAndRemoveUntil(
+          MaterialPageRoute(builder: (BuildContext context) => MyLogin()),
+          (Route<dynamic> route) => false);
+      this.succRegister = true;
     } else {
-      AlertBox.showAlertDialog(
-          context,
-          "Upss... wystapil problem z nawiazanie polaczenia",
-          response.statusCode.toString(),
-          "OK");
+      throw Exception(response.body);
     }
   }
 }
