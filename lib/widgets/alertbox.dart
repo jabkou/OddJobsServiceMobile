1638062f408511092b635773105245faf43f2502
90kid <dawidkoziej5@wp.pl>
2020-05-08 14:51:22 +0200

    Alertbox thats give info for user

--- lib/widgets/alertbox.dart
+++ lib/widgets/alertbox.dart
@@ -1,3 +1,25 @@
+import 'package:flutter/material.dart';
+
 class AlertBox{
-  
+    static showAlertDialog(BuildContext context, String title, String message, String buttonText) {
+    Widget okButton = FlatButton(
+      child: Text(buttonText),
+      onPressed: () {
+        Navigator.of(context, rootNavigator: true).pop('dialog');
+      },
+    );
+    AlertDialog alert = AlertDialog(
+      title: Text(title),
+      content: Text(message),
+      actions: [
+        okButton,
+      ],
+    );
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return alert;
+      },
+    );
+  }
 }
