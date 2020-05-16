c5f2e517963eb650d0a6efde58e83468952402af
90kid <dawidkoziej5@wp.pl>
2020-05-16 20:17:35 +0200

    add testScreen and comunication with widgets

--- lib/main.dart
+++ lib/main.dart
@@ -4,11 +4,11 @@ import 'package:flutterappservice/screens/catalog.dart';
 import 'package:flutterappservice/screens/first.dart';
 import 'package:flutterappservice/screens/login.dart';
 import 'package:flutterappservice/screens/register.dart';
+import 'package:flutterappservice/screens/testScreen.dart';
 import 'package:provider/provider.dart';
 
 import 'common/theme.dart';
-import 'models/cart.dart';
-import 'models/catalog.dart';
+import 'models/user.dart';
 
 
 void main() {
@@ -19,22 +19,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // Using MultiProvider is convenient when providing multiple objects.
-    return MultiProvider(
-      providers: [
-        // In this sample app, CatalogModel never changes, so a simple Provider
-        // is sufficient.
-        Provider(create: (context) => CatalogModel()),
-        // CartModel is implemented as a ChangeNotifier, which calls for the use
-        // of ChangeNotifierProvider. Moreover, CartModel depends
-        // on CatalogModel, so a ProxyProvider is needed.
-        ChangeNotifierProxyProvider<CatalogModel, CartModel>(
-          create: (context) => CartModel(),
-          update: (context, catalog, cart) {
-            cart.catalog = catalog;
-            return cart;
-          },
-        ),
-      ],
+    return ChangeNotifierProvider<User>(
+      create: (BuildContext context) => User.emptyUser(),
       child: MaterialApp(
         title: 'Provider Demo',
         theme: appTheme,
@@ -45,6 +31,7 @@ class MyApp extends StatelessWidget {
           '/catalog': (context) => MyCatalog(),
           '/cart': (context) => MyCart(),
           '/register': (context) => Register(),
+          '/testscreen': (context) => TestScreen(),
         },
       ),
     );
