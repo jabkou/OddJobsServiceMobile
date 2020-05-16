ab6c4421604b35165155d6843879062a4f6627c2
90kid <dawidkoziej5@wp.pl>
2020-05-16 20:18:01 +0200

    add update method

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -1,31 +1,81 @@
-class User{
+import 'package:flutter/cupertino.dart';
+
+class User with ChangeNotifier {
   String _userName;
   String _firstName;
   String _lastName;
   String _email;
   String _phoneNumber;
-  List _userProfilePhotoUrl;
+  List _userProfilePhotoUrl; //if they add
   bool _blocked;
-  User(this._userName, this._firstName, this._lastName, this._email, this._phoneNumber, this._userProfilePhotoUrl, this._blocked);
-  String getUserName(){
+  bool _login;
+  User.emptyUser() {
+    //named consturctor
+    this._login = false;
+  }
+  String getUserName() {
+    if (this._login && !this._blocked) throw Exception("blocked or not login");
     return _userName;
   }
-  String getFirstName(){
+
+  String getFirstName() {
+    if (this._login && !this._blocked) throw Exception("blocked or not login");
     return _firstName;
   }
-  String getLastName(){
+
+  String getLastName() {
+    if (this._login && !this._blocked) throw Exception("blocked or not login");
     return _lastName;
   }
-  String getEmail(){
+
+  String getEmail() {
+    if (this._login && !this._blocked) throw Exception("blocked or not login");
     return _email;
   }
-  String getPhoneNumber(){
+
+  String getPhoneNumber() {
+    if (this._login && !this._blocked) throw Exception("blocked or not login");
     return _phoneNumber;
   }
-  List getUserProfilePhotoUrl(){
+
+  List getUserProfilePhotoUrl() {
+    if (this._login && !this._blocked) throw Exception("blocked or not login");
     return _userProfilePhotoUrl;
   }
-  bool ifBlocked(){
+
+  bool isBlocked() {
     return _blocked;
+  }
+
+  bool isLogin() {
+    return _login;
+  }
+
+  void update(
+      {String userName,
+      String firstName,
+      String lastName,
+      String email,
+      String phoneNumber,
+      List userProfilePhotoUrl,
+      bool blocked,
+      bool login}) {
+    if (userName != null)
+      _userName = userName;
+    if (firstName != null)
+      _firstName = firstName;
+    if (lastName != null)
+      _lastName = lastName;
+    if (email != null)
+      _email = email;
+    if (phoneNumber != null)
+      _phoneNumber = phoneNumber;
+    if (userProfilePhotoUrl != null)
+      _userProfilePhotoUrl = userProfilePhotoUrl;
+    if (blocked != null)
+      _blocked = blocked;
+    if (login != null)
+      _login = login;
+    notifyListeners();
   }
 }
